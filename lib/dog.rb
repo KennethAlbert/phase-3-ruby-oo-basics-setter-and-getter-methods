58e2f1dd9b622070184619094040af8321ddb97d
Kenneth Albert <gichuka@gmail.com>
2022-08-19 02:47:14 +0300

    commit tree 06611353859ee9721881230de0aea7942fb6118a

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,9 @@
 class Dog
+    attr_accessor :name
+    attr_accessor :breed
+    def instilize=(name,breed)
+        @name = name
+        @breed = breed
+     end
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
+    attr_accessor :name
+    attr_accessor :job
+    def instilize=(name,job)
+        @name = name
+        @job= job
+     end
 
 end
